a2e13bfefabc6f741c557e66c01872cc084eb61d
Md Rifajul Islam (Rifaj) <146623093+rifaj007@users.noreply.github.com>
2024-12-24 21:52:31 +0600

    event update page added

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,19 +1,38 @@
 import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
+import { UpdateEventParams } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 
-const UpdateEvent = async () => {
-  const { userId }: { userId: string | null } = await auth();
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  const { sessionClaims, redirectToSignIn } = await auth();
+
+  const userId = sessionClaims?.userId as string | null;
+
+  const event = await getEventById(id);
+  
+  if (!userId) return redirectToSignIn();
 
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">
-          UpdateEvent
+          Update Event
         </h3>
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          userId={userId}
+          type="Update"
+          event={event}
+          eventId={event._id}
+        />
       </div>
     </>
   );
